4f2816120bf6cb9247f31fd6749c9ab6f9c1c82b
georgi <georgi.vorobiov@gmail.com>
2013-10-01 04:42:26 +0300

    ...

---
+++ Sumc.Models.Response/MainStop.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Sumc.Models.Response
+{
+    public class MainStop : Stop
+    {
+        public IEnumerable<string> TransportNumbers { get; set; }
+    }
+}

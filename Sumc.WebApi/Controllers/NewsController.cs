06ebf5a4e28abfad43395bde865d008a092c7beb
georgi <georgi.vorobiov@gmail.com>
2013-10-03 15:44:27 +0300

    ...

--- Sumc.WebApi/Controllers/NewsController.cs
+++ Sumc.WebApi/Controllers/NewsController.cs
@@ -27,14 +27,14 @@ namespace Sumc.WebApi.Controllers
                 var document = this.GetDocumentReader(text);
 
                 var news = new List<NewsGlanceInformation>();
-                var newsLinks = document.DocumentNode.SelectNodes("//page_box/a]");
-                var newsDates = document.DocumentNode.SelectNodes("//page_box/span[@class='date']");
+                var newsLinks = document.DocumentNode.SelectNodes("//*[@class='page_box']/a");
+                var newsDates = document.DocumentNode.SelectNodes("//*[@class='page_box']/span[@class='date']");
                 for (int i = 0; i < newsLinks.Count; i++)
                 {
                     var glanceNews = new NewsGlanceInformation();
-                    glanceNews.Id = newsLinks[i].Attributes.First(a => a.Name == "href").Value;
+                    glanceNews.Id = newsLinks[i].Attributes.First(a => a.Name == "href").Value.Substring(9);
                     glanceNews.Title = newsLinks[i].InnerHtml.Trim('\t', '\n');
-                    glanceNews.Date = DateTime.Parse(newsLinks[i].InnerHtml.Trim('\t', '\n'));
+                    glanceNews.Date =newsDates[i].InnerHtml.Trim('\t', '\n');
                     news.Add(glanceNews);
                 }
 
@@ -46,14 +46,14 @@ namespace Sumc.WebApi.Controllers
         {
             return this.PerformOperation<News>(() =>
             {
-                var response = this.newsRepository.GetNews(this.SessionKey, this.GlobalAuthKey);
+                var response = this.newsRepository.GetNews(id, this.SessionKey, this.GlobalAuthKey);
                 var text = this.GetTextFromResponse(response);
                 var document = this.GetDocumentReader(text);
 
                 var news = new News();
                 news.Title = document.DocumentNode.SelectSingleNode("//*[@class='page_title']").InnerText.Trim('\n', '\t');
-                news.Content = document.DocumentNode.SelectSingleNode("*[@class='page_cnt']").InnerText;
-                news.Date = DateTime.Parse(document.DocumentNode.SelectSingleNode("*[@class='date']").InnerText.Trim('\n', '\t'));
+                news.Content = document.DocumentNode.SelectSingleNode("//*[@class='page_cnt']").InnerText;
+                news.Date = document.DocumentNode.SelectSingleNode("//*[@class='date']").InnerText.Trim('\n', '\t');
                 return news;
             });
         }

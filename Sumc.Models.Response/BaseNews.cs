5db16545cbbcf09cb6f82f828d35fae673b2af08
georgi <georgi.vorobiov@gmail.com>
2013-10-03 15:48:49 +0300

    ...

---
+++ Sumc.Models.Response/BaseNews.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Sumc.Models.Response
+{
+    public abstract class BaseNews
+    {
+        public string Title { get; set; }
+
+        public string Date { get; set; }
+    }
+}
---
+++ Sumc.Repositories/NewsRepository.cs
@@ -0,0 +1,25 @@
+﻿using Sumc.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sumc.Repositories
+{
+    public class NewsRepository :BaseRequestRepository
+    {
+        public HttpWebResponse GetNews(Cookie sessionToken, Cookie authToken)
+        {
+            var url = string.Format("{0}bg/news/p/1", BaseSofiaTrafficUrl);
+            return HttpRequester.Get(url, sessionToken, authToken);
+        }
+        //id == real url
+        public HttpWebResponse GetNews(string id, Cookie sessionToken, Cookie authToken)
+        {//891/avtobusna-liniia-107
+            var url = string.Format("{0}bg/news/{1}", BaseSofiaTrafficUrl, id);
+            return HttpRequester.Get(url, sessionToken, authToken);
+        }
+    }
+}

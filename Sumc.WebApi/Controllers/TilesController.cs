0eb054603a800ef75cbcffe4566755086e39d545
georgi <georgi.vorobiov@gmail.com>
2013-10-04 18:55:59 +0300

    ...

--- Sumc.WebApi/Controllers/TilesController.cs
+++ Sumc.WebApi/Controllers/TilesController.cs
@@ -52,5 +52,30 @@ namespace Sumc.WebApi.Controllers
 
             return new HttpResponseMessage() { Content = new StringContent(tile, Encoding.UTF8, "application/xml") };
         }
+
+        public HttpResponseMessage Get()
+        {
+            var newsController = new NewsController();
+           // var news = newsController.GetNews();
+                        
+            var tileXml = @"<?xml version=""1.0"" encoding=""utf-8"" ?><tile>
+                       <visual>
+                         <binding template=""TileWideImageAndText02"">
+                          <image id=""1"" src=""image1.png"" alt=""Моят транспорт""/>
+                          <text id=""1"">Text Field 1</text>
+                          <text id=""2"">Text Field 2</text>
+                        </binding>
+                         <binding template=""TileSquareBlock"">
+                            <text id=""1"">{4}</text>
+                            <text id=""2"">{6}</text>
+                         </binding> 
+                       </visual>
+                     </tile>";
+
+            //var firstNews = news
+            var tile = string.Format(tileXml);
+
+            return new HttpResponseMessage() { Content = new StringContent(tile, Encoding.UTF8, "application/xml") };
+        }
     }
 }
